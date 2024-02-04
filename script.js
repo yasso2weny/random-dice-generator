ac4c080e998fbc28088a747b0cf146a1434891f4
YassinMohamed <52466287+yasso2weny@users.noreply.github.com>
2024-02-04 10:18:28 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Random number from 1 - 6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+// images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+// Random number from 1 - 6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// images/dice1.png - images/dice6.png
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
